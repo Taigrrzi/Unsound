1537ca3f734b50e73e37a2b99cbb36d8255d1f49
Taigrrzi <tombatchelor12@yahoo.co.uk>
2015-09-22 00:51:26 +0100

    Initial Commit
    Add Original Build, function ship with thrusters.

---
+++ Assets/Scripts/RocketBrain.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketBrain : MonoBehaviour {
+
+    public bool active;
+    public float thrustPower;
+    //public byte rocketDirection; //0 = right, 1 = down, 2 = left, 3 = up
+    public float myRotation;
+    public float xComponent;
+    public float yComponent;
+    public float xDistance;
+    public float yDistance;
+    // Update is called once per frame
+    void Start()
+    {
+        yDistance = transform.localPosition.y - transform.parent.position.y;
+        xDistance = transform.localPosition.x - transform.parent.position.x;
+        xComponent = -Mathf.Sin(Mathf.Deg2Rad * myRotation);
+        yComponent = Mathf.Cos(Mathf.Deg2Rad * myRotation);
+    }
+
+    void Update () {
+        myRotation = transform.parent.rotation.eulerAngles.z;
+        GetComponent<Animator>().SetBool("RocketActive",active);
+        if (active)
+        {
+            transform.parent.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(thrustPower*xComponent,thrustPower*yComponent),ForceMode2D.Force);
+            transform.parent.GetComponent<Rigidbody2D>().AddTorque(0.1f*((thrustPower*xComponent*yDistance) + (thrustPower * yComponent * xDistance)));
+        }
+	}
+}
---
+++ Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFollow : MonoBehaviour
+{
+
+    public GameObject target;
+    public float zoomSpeed;
+    public float intendedZoom;
+    public Vector2 mouseScreenPosition;
+    public Vector2 mouseWorldPosition;
+    public Vector3 intendedPosition;
+
+    void FixedUpdate()
+    {
+        intendedZoom -= zoomSpeed * Input.GetAxis("MouseScrollWheel");
+        if (intendedZoom < 2)
+        {
+            intendedZoom = 2;
+        }
+        if (intendedZoom > 16)
+        {
+            intendedZoom = 16;
+        }
+        //transform.position = new Vector3 (target.transform.position.x, target.transform.position.y, Mathf.Lerp(transform.position.z,intendedZoom,0.2f));
+        if (Input.GetButton("Look"))
+        {
+            mouseScreenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mouseScreenPosition.x, mouseScreenPosition.y, 0f));
+            intendedPosition = new Vector3((target.transform.position.x + mouseWorldPosition.x) / 2, (target.transform.position.y + mouseWorldPosition.y) / 2, -1);
+        }
+        else
+        {
+            intendedPosition = new Vector3(target.transform.position.x, target.transform.position.y, -1);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, intendedPosition, Mathf.Abs(((17 - intendedZoom) / 16) - (intendedZoom / (intendedZoom + 1)) / 4) + 0.1f);
+        GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, intendedZoom, 0.2f);
+    }
+}
