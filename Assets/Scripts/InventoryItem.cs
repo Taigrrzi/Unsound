c2d36d77e6125e060d0ba259076a71395478b9e7
Taigrrzi <tombatchelor12@yahoo.co.uk>
2015-12-13 16:15:51 +0000

    Changes to asteroid variance and begin inventory

---
+++ Assets/Scripts/InventoryItem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryItem {
+
+    public string type;
+    public string subType;
+    public Sprite image;
+    public int size;
+
+    public InventoryItem(string ty, string subty,Sprite img,int si)
+    {
+        subType = subty;
+        type = ty;
+        img = image;
+        size = si;
+    }
+
+}
---
+++ Assets/Scripts/destroyAtDistance.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyAtDistance : MonoBehaviour {
+    public GameObject target;
+    public float distance;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.Find("PlayerShip");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Vector3.Distance(transform.position,target.transform.position)<distance)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
